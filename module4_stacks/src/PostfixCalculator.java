2a66bd3ecbd0c06bf209b9093bef42abc6a07577
corwin-bell <corwin.m.bell@gmail.com>
2024-10-05 11:47:06 -0600

    Refactor of postfix calculator using array or postfix items

--- module4_stacks/src/PostfixCalculator.java
+++ module4_stacks/src/PostfixCalculator.java
@@ -1,18 +1,18 @@
 import java.util.Arrays;
 import java.util.Stack;
-import java.util.StringTokenizer;
 
 public class PostfixCalculator {
 
     // members
-    private StringTokenizer postfixTokens;
     private Stack<Double> postfixStack;
 
     // Default constructor
-    public PostfixCalculator() {}
+    public PostfixCalculator() {
+        this.postfixStack = new Stack<>();
+    }
     // methods
-    // check if token is parseable to double
-    private boolean isDouble (String str) {
+    // isDouble method
+    private boolean isDouble(String str) {
         try {
             Double.parseDouble(str);
             return true;
@@ -20,53 +20,55 @@ public class PostfixCalculator {
             return false;
         }
     }
-    // TODO: refactor based on this: https://www.geeksforgeeks.org/evaluation-of-postfix-expression/
-    public Double calculate(String postfixStr) {
-        try {
-            postfixTokens = new StringTokenizer (postfixStr, " ");
-            String token = "";  
-            String[] operatorArray = {"+", "-", "*", "/", "%"};
-            Double operand1;
-            Double operand2;
-            // TODO: refactor as split of string into string array
-            while (postfixTokens.hasMoreTokens()) {
-                token = postfixTokens.nextToken();
-                System.out.println("token: " + token);
-                // // if term is a double operand, push to stack, need to check validity
-                if (isDouble(token)) {
-                    System.out.println("is double: " + token);
-                    postfixStack.push(Double.valueOf(token));
-                }
-                // else if (Arrays.asList(operatorArray).contains(token)) {
-                //     System.out.println("is operator: " + token);
-                //     operand2 = postfixStack.pop();
-                //     operand1 = postfixStack.pop();
-                //     switch (token) {
-                //         case "+":
-                //             postfixStack.push(operand1 + operand2);
-                //             break;
-                //         default:
-                //             break;
-                //     } 
-                //     // pop operands off stack,
-                //     // perform operation
-                //     // push result back to stack
-                //     System.out.println("TODO: add calculate method");
-                // }
-                // else {
-                //     // throw error since it's an invalid string
-                //     continue;
-                // }
-            }
-            System.out.println("result: " + postfixStack.peek());
-            return postfixStack.pop();    
-        } catch (Exception e) {
-            // TODO: handle exception
-            return -1.0;
+    // operate method
+    private Double operate(Double operand1, Double operand2, String operator) {
+        // add try catch for divide by zero and other weird math behavior
+        switch (operator) {
+            case "+":
+            return operand2 + operand1;
+            case "-":
+            return operand2 - operand1;
+            case "*":
+            return operand2 * operand1;
+            case "/":
+            return operand2 / operand1;
+            case "%":
+            return operand2 % operand1;
+            default:
+                return -1.0;
         }
-
     }
-        // try handle invalid postfixExpression error
-            
-        // catch invalid postfixExpression, print error message
+    public Double calculate(String postfixStr) {  
+        // split postfix str to an array of postfix items
+        String[] postfixArray = postfixStr.split(" ");    
+        // store array of operators to test
+        String[] operatorArray = {"+", "-", "*", "/", "%"};
+        // declare operand variables to evaluate postfix expressions
+        Double operand1;
+        Double operand2;
+        // for each item in array
+        for (String item: postfixArray) {
+            if (isDouble(item)) {
+                postfixStack.push(Double.parseDouble(item));
+                System.out.println("item: " + postfixStack.peek() + " added to stack.");
+            }
+            else if (Arrays.asList(operatorArray).contains(item)) {
+                // try catch for EmptyStackException
+                operand1 = postfixStack.pop();
+                operand2 = postfixStack.pop();
+                postfixStack.push(operate(operand1, operand2, item));
+                System.out.println(operand2 + item + operand1 + " = " + postfixStack.peek() + " added to stack");
+            }
+            else {
+                System.out.println("Invalid Postfix");
+            }
+        }
+            // if double, push to stack
+            // if operator, pop last two stack items, operate, push result to stack
+            // else, invalid input   
+        System.out.println("result: " + postfixStack.peek());
+        return postfixStack.pop();    
+    }
+
 }
+
