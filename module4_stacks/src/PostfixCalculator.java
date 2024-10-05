79a2886a33f48deb646b0e5f86fd1dfff5589e08
corwin-bell <corwin.m.bell@gmail.com>
2024-10-05 11:07:08 -0600

    Add string tokenizer approach, not working for me, but saving in case I want to go back

--- module4_stacks/src/PostfixCalculator.java
+++ module4_stacks/src/PostfixCalculator.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
@@ -19,22 +20,45 @@ public class PostfixCalculator {
             return false;
         }
     }
+    // TODO: refactor based on this: https://www.geeksforgeeks.org/evaluation-of-postfix-expression/
     public Double calculate(String postfixStr) {
         try {
-            postfixTokens = new StringTokenizer (postfixStr, " +-*/", true);
+            postfixTokens = new StringTokenizer (postfixStr, " ");
             String token = "";  
+            String[] operatorArray = {"+", "-", "*", "/", "%"};
+            Double operand1;
+            Double operand2;
+            // TODO: refactor as split of string into string array
             while (postfixTokens.hasMoreTokens()) {
                 token = postfixTokens.nextToken();
-                System.out.println(token);
-                // if term is a float operand, push to stack, need to check validity
+                System.out.println("token: " + token);
+                // // if term is a double operand, push to stack, need to check validity
                 if (isDouble(token)) {
+                    System.out.println("is double: " + token);
                     postfixStack.push(Double.valueOf(token));
                 }
+                // else if (Arrays.asList(operatorArray).contains(token)) {
+                //     System.out.println("is operator: " + token);
+                //     operand2 = postfixStack.pop();
+                //     operand1 = postfixStack.pop();
+                //     switch (token) {
+                //         case "+":
+                //             postfixStack.push(operand1 + operand2);
+                //             break;
+                //         default:
+                //             break;
+                //     } 
+                //     // pop operands off stack,
+                //     // perform operation
+                //     // push result back to stack
+                //     System.out.println("TODO: add calculate method");
+                // }
+                // else {
+                //     // throw error since it's an invalid string
+                //     continue;
+                // }
             }
-            // if operator (+, -, *, /, %), 
-                // pop operands off stack,
-                // perform operation
-                // push result back to stack
+            System.out.println("result: " + postfixStack.peek());
             return postfixStack.pop();    
         } catch (Exception e) {
             // TODO: handle exception
