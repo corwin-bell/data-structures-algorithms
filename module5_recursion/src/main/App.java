97d9feef265205e121c5cba7e22352c29c690ac9
corwin-bell <corwin.m.bell@gmail.com>
2024-10-27 10:52:14 -0400

    Add try catch blocks for more informative error handling

--- module5_recursion/src/main/App.java
+++ module5_recursion/src/main/App.java
@@ -1,8 +1,22 @@
 package main;
-
 public class App {
     public static void main(String[] args) {
-        int[] testArray = {783, 99, 472, 182, 264, 543, 356, 295, 692, 491, 94};
-        IntRadixSorter.intRadixSort(testArray);
+        
+        try {
+            // int[] testArray = {783, 99, 472, 182, 264, 543, 356, 295, 692, 491, 94};
+            int[] testArray = null;
+            IntRadixSorter.intRadixSort(testArray);    
+        } 
+        catch (NullPointerException e) {
+            System.err.println(e.getMessage());
+            System.out.println("Please instantiate input array with at least one integer.");
+        }
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println(e.getMessage());
+            System.out.println("Please instantiate input array with at least one integer.");
+        }
+
+        
+        
     }
 }
